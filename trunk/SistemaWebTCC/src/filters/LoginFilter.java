6f62ba329741a21dcbaef4f3e47a45abbdd12d3f
deiwys@gmail.com <deiwys@gmail.com@e2569139-5c89-cd5e-59a1-37004c961a7b>
2013-09-02 20:48:51 +0000

    Commit!

---
+++ trunk/SistemaWebTCC/src/filters/LoginFilter.java
@@ -0,0 +1,55 @@
+package filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+public class LoginFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public LoginFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		Object usuario = httpRequest.getSession().getAttribute("usuario");
+		System.out.println("FILTER = "+usuario);
+		if(usuario == null || usuario.toString().equals("")){
+			httpResponse.sendRedirect(httpRequest.getContextPath()+"/index.html");
+			return;
+		}
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
